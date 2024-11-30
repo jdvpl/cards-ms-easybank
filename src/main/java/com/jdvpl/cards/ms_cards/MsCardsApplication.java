2c5a5cef15d8c4ed26684c30b5758b78289ec5dd
jdsuarez <jdsuarez@fgs.co>
2024-11-30 11:24:42 -0500

    Update dependencies, add JPA auditing, and implement CardsController for CRUD operations

--- src/main/java/com/jdvpl/cards/ms_cards/MsCardsApplication.java
+++ src/main/java/com/jdvpl/cards/ms_cards/MsCardsApplication.java
@@ -2,8 +2,17 @@ package com.jdvpl.cards.ms_cards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(info = @Info(title = "Cards microservice REST API Documentation", description = "EazyBank Cards microservice REST API Documentation", version = "v1", contact = @Contact(name = "Madan Reddy", email = "jdvpl@gmail.com", url = "https://portfolio-jdvpl.vercel.app/"), license = @License(name = "Apache 2.0", url = "https://portfolio-jdvpl.vercel.app/")), externalDocs = @ExternalDocumentation(description = "EazyBank Cards microservice REST API Documentation", url = "https://www.eazybytes.com/swagger-ui.html"))
 public class MsCardsApplication {
 
 	public static void main(String[] args) {

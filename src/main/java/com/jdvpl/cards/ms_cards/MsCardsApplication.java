a62b221b1eacc199951a9eab99c646d91732712d
jdsuarez <jdsuarez@fgs.co>
2024-11-30 10:17:46 -0500

    initial ms loans

---
+++ src/main/java/com/jdvpl/cards/ms_cards/MsCardsApplication.java
@@ -0,0 +1,13 @@
+package com.jdvpl.cards.ms_cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jdvpl/cards/ms_cards/MsCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdvpl.cards.ms_cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
